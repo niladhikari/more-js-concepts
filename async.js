7c73f8e0873089085e335cfad5c6c587c8664c25
niladhikari <niladhikari445@gmail.com>
2023-08-22 19:59:16 +0600

    add the async and promise code

---
+++ async.js
@@ -0,0 +1,17 @@
+// console.log(1);
+// console.log(2);
+// setTimeout(doSomething,2000);
+// console.log(4);
+// console.log(5);
+
+// function doSomething(params) {
+//     console.log(3)
+// }
+
+
+console.log('i');
+setTimeout(doSomething,2000);
+console.log('Ice-creme');
+function doSomething(params) {
+        console.log("eat")
+    }
